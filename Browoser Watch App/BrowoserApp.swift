251448dd4f027584c4b9fc201bbe1544546b2934
りん <31785428+rin-senpai@users.noreply.github.com>
2023-03-31 17:40:28 +0800

    Initial Commit

---
+++ Browoser Watch App/BrowoserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowoserApp.swift
+//  Browoser Watch App
+//
+//  Created by 小鳥遊りん on 31/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Browoser_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Browoser Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Browoser Watch App
+//
+//  Created by 小鳥遊りん on 31/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
