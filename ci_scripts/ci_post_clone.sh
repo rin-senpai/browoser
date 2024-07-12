d455df838219d52f498ab542d83e676408bc53cd
りん <31785428+rin-senpai@users.noreply.github.com>
2024-07-12 16:27:19 +1000

    Actually trolling

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,4 +4,11 @@
 #  Browoser
 #
 #  Created by りん on 12/7/2024.
-#  
+#
+
+if [[-n $GEMINI_API_KEY]];
+then
+    cd "Browoser Watch App/APIKey"
+    /usr/libexec/PlistBuddy "GenerativeAI-Info.plist"
+    /usr/libexec/PlistBuddy -c "add API_KEY string $GEMINI_API_KEY" GenerativeAI-Info.plist
+fi
